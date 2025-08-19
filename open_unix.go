9d859de157bd8b23871cf912c12393f8a4ce1d91
Alex Efros <powerman@powerman.name>
2025-08-20 01:12:28 +0300

    chore: drop macos fixes

--- open_unix.go
+++ open_unix.go
@@ -2,23 +2,10 @@
 
 package tail
 
-import (
-	"io/fs"
-	"os"
-	"runtime"
-)
+import "os"
 
 // openFile opens a file for reading in a platform-specific way.
 func openFile(path string) (*os.File, error) {
-	// On macOS, opening FIFO with O_RDONLY blocks until a writer opens the write end.
-	// For FIFO files, use O_RDWR to avoid blocking.
-	if runtime.GOOS == "darwin" {
-		fi, err := os.Lstat(path)
-		if err == nil && fi.Mode()&fs.ModeNamedPipe != 0 {
-			// This is a FIFO on macOS, open with O_RDWR to avoid blocking
-			return os.OpenFile(path, os.O_RDWR, 0) //nolint:gosec // Path comes from trusted sources.
-		}
-	}
 	return os.Open(path) //nolint:gosec // Path comes from trusted sources.
 }
 
