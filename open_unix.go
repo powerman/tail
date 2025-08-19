4c90744562cba823fbd287ebc12651b2bd238ce9
Alex Efros <powerman@powerman.name>
2025-08-20 00:45:50 +0300

    Revert "fix(test): enable fifo on macos"
    This reverts commit 7306699e4da1bfd456e97876a7410f5368de6b0a.

--- open_unix.go
+++ open_unix.go
@@ -2,24 +2,10 @@
 
 package tail
 
-import (
-	"io/fs"
-	"os"
-	"runtime"
-)
+import "os"
 
 // openFile opens a file for reading in a platform-specific way.
 func openFile(path string) (*os.File, error) {
-	// On macOS, FIFO (named pipes) opened with O_RDONLY will block until
-	// someone opens the write end. To avoid this, we check if the file is a FIFO
-	// and open it with O_RDWR instead.
-	if runtime.GOOS == "darwin" {
-		fi, err := os.Stat(path)
-		if err == nil && fi.Mode()&fs.ModeNamedPipe != 0 {
-			// This is a FIFO on macOS, open with O_RDWR to avoid blocking
-			return os.OpenFile(path, os.O_RDWR, 0) //nolint:gosec // Path comes from trusted sources.
-		}
-	}
 	return os.Open(path) //nolint:gosec // Path comes from trusted sources.
 }
 
