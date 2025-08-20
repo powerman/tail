98a8f8c99dd85834b8ee6557a0d22fe1b58378d0
Alex Efros <powerman@powerman.name>
2025-08-20 21:48:33 +0300

    fix: FIFO hang on macOS (#41)
    Fixes #14

--- open_unix.go
+++ open_unix.go
@@ -2,11 +2,25 @@
 
 package tail
 
-import "os"
+import (
+	"os"
+
+	"golang.org/x/sys/unix"
+)
 
 // openFile opens a file for reading in a platform-specific way.
 func openFile(path string) (*os.File, error) {
-	return os.Open(path) //nolint:gosec // Path comes from trusted sources.
+	// Open the FIFO in read-write non-blocking mode:
+	// - O_RDWR ensures we never get EOF when reading because at least one writer is present.
+	// - O_NONBLOCK is required to have Close() interrupt the Read() call.
+	// - Also one of them is required to avoid blocking on Open() FIFO without writers.
+	// This shouldn't affect regular files (as long as we're not trying to write, of course).
+	fd, err := unix.Open(path, unix.O_RDWR|unix.O_NONBLOCK, 0)
+	if err != nil {
+		return nil, err
+	}
+	f := os.NewFile(uintptr(fd), path)
+	return f, nil
 }
 
 // createTempFile creates a temporary file in a platform-specific way.
